acb253539e42d8ff721cfc01c534a1bb3e71bb9b
Aryan566 <aryansinha566@gmail.com>
2022-10-12 13:22:48 +0530

    db properties added

--- src/test/java/com/smallChange/integration/UserDaoImplTest.java
+++ src/test/java/com/smallChange/integration/UserDaoImplTest.java
@@ -7,29 +7,37 @@ import java.sql.Connection;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 
 class UserDaoImplTest {
 	
 	SimpleDataSource sds;
-	ClientDao dao;
-	JdbcTemplate jdbcTemplate;
-	DbTestUtils dbTestUtils;
+	UserDao dao;
 	Connection connection;
 	TransactionManager manager;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		sds = new SimpleDataSource();
+		
+		connection = sds.getConnection();
+						
+		// Start the transaction
+		dao = new UserDaoImpl(sds);
+		manager = new TransactionManager(sds);
+		manager.startTransaction();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		manager.rollbackTransaction();
+		sds.shutdown();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNotNull() {
+		assertNotNull(dao);
 	}
 
 }
